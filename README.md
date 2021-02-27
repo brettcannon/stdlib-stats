fa87c5dadda625cf590a51855a6ce94c456ccac4
Brett Cannon <brett@python.org>
2021-02-27 13:49:38 -0800

    Calculate mapping of public modules to private modules

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # stdlib-stats
-Various statistics on Python's standard library
+
+Various statistics on Python's standard library.
+
+## Statistics
+
+### Required for startup
+
+Determined by whether the module is used to run `-S -c "pass"`.
+
+### LOC
+
+XXX
+
+### Open PRs
+
+XXX take into account module age
+
+### Commits
+
+XXX take into account module age
+
+### Usage in the community
+
+XXX
+
+## Technical
+
+### Connecting private modules to their public dependents
+
+`private_modules.json` maps public modules to any private modules they depend
+on. For modules that are "cheating" and using private modules directly instead
+of their equivalent public API, they not listed as a dependent
+(e.g. `multiprocessing` directly using `_weakrefset` instead of going through
+`weakref`).
+
+Data is verified via `private_modules.py`.
+
+### Mapping paths to modules
+
+XXX
+
+### Counting LOC
+
+XXX
+
+### Module age
+
+XXX
+
+### Collecting module usage on GitHub
+
+XXX
+
+### Usage by the community
+
+XXX
---
+++ private_modules.py
@@ -0,0 +1,46 @@
+import json
+import pathlib
+import sys
+
+
+repo_location = pathlib.Path(sys.argv[1])
+library_docs = repo_location / "Doc" / "library"
+public_modules = {
+    "lib2to3",  # In 2to3.rst
+    "this",  # Easter egg
+    "antigravity",  # Easter egg
+    "cProfile",  # In profile.rst
+    "pstats",  # In profile.rst
+    "turtledemo",  # In turtle.rst
+    "encodings",  # In codecs.rst
+    "idlelib",  # For IDLE
+    "_xxsubinterpreters",  # For testing subinterpreters
+    }
+
+for item in library_docs.iterdir():
+    filename = item.name
+    if not filename.endswith(".rst") or filename == "pyexpat.rst":
+        continue
+
+    name = filename.partition(".")[0]
+    if name in sys.stdlib_module_names:
+        public_modules.add(name)
+
+public_modules = frozenset(public_modules)
+private_modules = sys.stdlib_module_names - public_modules
+
+with open("private_modules.json", "r") as file:
+    module_mapping = json.load(file)
+written_public_modules = frozenset(module_mapping.keys())
+if diff := written_public_modules - public_modules:
+    print("Modules recorded as public but are private:", diff)
+    sys.exit(1)
+elif diff := public_modules - written_public_modules:
+    print("Public modules not recorded:", diff)
+
+written_private_modules = frozenset(name for names in module_mapping.values()
+                                            for name in names)
+if diff := private_modules - written_private_modules:
+    print("Private modules not recorded:", diff)
+elif diff := written_private_modules - private_modules:
+    print("Modules misclassified as private:", diff)
