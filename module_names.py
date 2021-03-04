40b1e9f1e29c0564280bef6ac1b2ba95abed3677
Brett Cannon <brett@python.org>
2021-03-03 20:06:53 -0800

    Generate list of module names

---
+++ module_names.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.10
+"""Dump `sys.stdlib_module_names` to `module_names.json`."""
+
+import json
+import sys
+
+
+with open("module_names.json", "w", encoding="utf-8") as file:
+    json.dump(sorted(sys.stdlib_module_names), file)
