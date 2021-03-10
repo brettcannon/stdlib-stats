7b05b3bd6da7e7973ac47704e05513bc01c717f8
Brett Cannon <brett@python.org>
2021-03-09 17:23:07 -0800

    Strip _xxsubinterpreters from data

--- module_names.py
+++ module_names.py
@@ -4,6 +4,8 @@
 import json
 import sys
 
+module_names = set(sys.stdlib_module_names)
+module_names.remove("_xxsubinterpreters")
 
 with open("module_names.json", "w", encoding="utf-8") as file:
-    json.dump(sorted(sys.stdlib_module_names), file, indent=2)
+    json.dump(sorted(module_names), file, indent=2)
