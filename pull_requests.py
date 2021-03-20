6ed4c6a526731edad946769a5ce445a998485e65
Brett Cannon <brett@python.org>
2021-03-19 17:21:23 -0700

    Get file paths as part of the PR request for perf reasons

--- pull_requests.py
+++ pull_requests.py
@@ -23,7 +23,7 @@ def grouper(iterable, n, fillvalue=None):
     return itertools.zip_longest(*args, fillvalue=fillvalue)
 
 
-async def changed_files(gh, pr_id, paths):
+async def changed_files(gh, pr_id, cursor, paths):
     """Append file paths for the changed PR to `paths`."""
     has_more_files = True
     cursor = None
@@ -42,7 +42,8 @@ async def main():
     async with httpx.AsyncClient(timeout=None) as client:
         gh = gidgethub.httpx.GitHubAPI(client, "brettcannon/stdlib-stats",
                                        oauth_token=oauth_token)
-        pr_ids = []
+        pr_to_files = {}
+        more_files = []
         has_more_prs = True
         cursor = None
         print("Getting PR IDs ...", end="", flush=True)
@@ -56,16 +57,25 @@ async def main():
                 pr_node = pr["node"]
                 pr_number = pr_node["number"]
                 pr_id = pr_node["id"]
-                pr_ids.append((pr_number, pr_id))
-        print(len(pr_ids), "PRs found")
+                files_connection = pr_node["files"]
+                files = []
+                files_cursor = None
+                for files_edge in files_connection["edges"]:
+                    files_cursor = files_edge["cursor"]
+                    files.append(files_edge["node"]["path"])
+                pr_to_files[pr_number] = files
+                if files_connection["pageInfo"]["hasNextPage"]:
+                    more_files.append((gh, pr_id, files_cursor, files))
+        print(len(pr_to_files), "PRs found")
 
-        pr_to_files = {}
-        for pr_chunk in grouper(pr_ids, 10):
+        print(len(more_files), "PRs have more files", end="", flush=True)
+        # Throttling to 10 concurrent requests every 5 seconds is a total guess,
+        # but it seems to not trigger GitHub's abuse defenses.
+        for pr_chunk in grouper(more_files, 10):
             async with curio.TaskGroup() as g:
-                for pr_number, pr_id in pr_chunk:
-                    files = []
-                    pr_to_files[pr_number] = files
-                    await g.spawn(changed_files, gh, pr_id, files)
+                for args in filter(None, pr_chunk):
+                    await g.spawn(changed_files, *args)
+                await g.spawn(curio.sleep, 5)
             print(".", end="", flush=True)
 
         with open("pull_requests.json", "w", encoding="utf-8") as file:
